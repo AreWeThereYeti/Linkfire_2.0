a51aa47e74ca536f8e8ce0c929366a4fee0d33d1
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-06-16 13:47:57 +0200

    added gulp server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp'),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		connect = require('gulp-connect'),
+	  compass = require('gulp-compass'),
+		sass = require('gulp-ruby-sass'),
+		gutil = require('gulp-util');
+
 
 gulp.task('minify', function () {
 	gulp.src('dev/scripts/**/*.js')
 			.pipe(uglify())
 			.pipe(gulp.dest('dist'))
 });
+
+
+//Start webserver on localhost /localhost:3000
+gulp.task('webserver', function() {
+	console.log('console : starting webserver');
+	connect.server({
+//		Start project from "dev" folder
+		root : ['dev'],
+		port: process.env.PORT || 3000,
+		livereload: true,
+		open: {
+			file: 'dev/index.html',
+			browser: 'Google Chrome'
+		}
+	});
+});
+
+gulp.task('default', ['webserver']);
