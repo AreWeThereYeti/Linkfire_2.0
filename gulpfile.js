cc77e4019dcc26fcff4ccd0d5f18194ccdb2e0e3
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-07-21 16:59:44 +0200

    new gulp script for dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,58 @@ var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		connect = require('gulp-connect'),
 		cssmin = require('gulp-cssmin'),
-		sass = require('gulp-ruby-sass'),
-		compass = require('gulp-compass'),
 		gutil = require('gulp-util'),
 		concat = require('gulp-concat'),
 		rename    = require('gulp-rename'),
 		watch = require('gulp-watch'),
-		livereload = require('gulp-livereload');
+		livereload = require('gulp-livereload'),
+		ngAnnotate = require('gulp-ng-annotate'),
+		clean = require('gulp-clean'),
+		minifyHTML = require('gulp-minify-html');
 
-
-gulp.task('minify', function () {
-	gulp.src('dev/scripts/**/*.js')
-			.pipe(uglify())
-			.pipe(gulp.dest('.tmp/'))
+//Clean tmp folder after tasks
+gulp.task('clean', function () {
+	return gulp.src('.tmp/', {read: false})
+			.pipe(clean());
 });
 
-gulp.task('concat', function() {
-	gulp.src('.tmp/**/*.js')
-			.pipe(concat('all.js'))
+//Move html and minify
+gulp.task('minify-html', function() {
+	var opts = {comments:true,spare:true};
+	gulp.src('./dev/*.html')
+			.pipe(minifyHTML({
+				empty:true
+			}))
+			.pipe(gulp.dest('./dist/'))
+});
+
+//Minify js and move to dist
+gulp.task('minify', function () {
+	gulp.src('dev/scripts/**/*.js')
+			.pipe(ngAnnotate())
+			.pipe(uglify('app.js', {
+				mangle: false,
+				output: {
+					beautify: true
+				}
+			}))
 			.pipe(gulp.dest('./dist/scripts'))
+});
+
+//Move html and minify
+gulp.task('minify-template-html', function() {
+	var opts = {comments:true,spare:true};
+	gulp.src('./dev/scripts/templates/**/*.html')
+			.pipe(minifyHTML({
+				empty:true
+			}))
+			.pipe(gulp.dest('./dist/scripts/templates'))
 });
 
 gulp.task('css', function () {
 	gulp.src('dev/stylesheets/**/*.css')
 			.pipe(cssmin())
+			.pipe(concat('main.css'))
 			.pipe(rename({suffix: '.min'}))
 			.pipe(gulp.dest('dist/stylesheets'));
 });
@@ -47,4 +75,4 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('default', ['webserver', 'watch']);
-gulp.task('build', ['minify', 'concat', 'css' ]);
+gulp.task('build', ['minify', 'css', 'minify-html', 'minify-template-html' ]);
