c20b9f5dcaf95ea10073b4dcb64eedc91b07edc1
Niels Peter Rasmussen <nprasmussen@gmail.com>
2014-07-30 16:42:02 +0200

    updated autocomplete example function

--- dev/scripts/controllers/CreateBoardCtrl.js
+++ dev/scripts/controllers/CreateBoardCtrl.js
@@ -1,19 +1,22 @@
 'use strict';
-linkfireWebappApp.controller('CreateBoardCtrl', function ($scope, $http) {
+linkfireWebappApp.controller('CreateBoardCtrl', function ($scope, $http, $q) {
     $scope.tags = [];
 
-    $scope.autos = function(){
+    $scope.autocomplete_options = [
+        { "text": "nr@linkfire.com" },
+        { "text": "le@linkfire.com" },
+        { "text": "mm@linkfire.com" },
+        { "text": "jj@linkfire.com" },
+        { "text": "ja@linkfire.com" }
+    ];
 
-        return [
-            { "text": "nr@linkfire.com" },
-            { "text": "le@linkfire.com" },
-            { "text": "mm@linkfire.com" },
-            { "text": "jj@linkfire.com" },
-            { "text": "ja@linkfire.com" }
-        ];
-
+    $scope.autocomplete_example = function(){
+        var deferred = $q.defer();
+        deferred.resolve($scope.autocomplete_options);
+        return deferred.promise;
     };
 
+    //fetching autocompletes from json
     $scope.loadTags = function(query) {
         return $http.get('tags.json');
     };
