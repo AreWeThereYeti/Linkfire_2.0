09040d70d21a45e6207e917770b05d6cf4ed54ad
Niels Peter Rasmussen <nprasmussen@gmail.com>
2014-07-30 16:22:25 +0200

    add members + autocomplete

--- dev/scripts/controllers/CreateBoardCtrl.js
+++ dev/scripts/controllers/CreateBoardCtrl.js
@@ -1,6 +1,19 @@
 'use strict';
 linkfireWebappApp.controller('CreateBoardCtrl', function ($scope, $http) {
     $scope.tags = [];
+
+    $scope.autos = function(){
+
+        return [
+            { "text": "nr@linkfire.com" },
+            { "text": "le@linkfire.com" },
+            { "text": "mm@linkfire.com" },
+            { "text": "jj@linkfire.com" },
+            { "text": "ja@linkfire.com" }
+        ];
+
+    };
+
     $scope.loadTags = function(query) {
         return $http.get('tags.json');
     };
