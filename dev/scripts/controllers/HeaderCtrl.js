ed3ba415171fd8e180654df9b2d49b6e07939fc3
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-08-01 11:01:22 +0200

    fixed header controller

--- dev/scripts/controllers/HeaderCtrl.js
+++ dev/scripts/controllers/HeaderCtrl.js
@@ -6,60 +6,47 @@ linkfireWebappApp.controller('HeaderCtrl', ['$scope', 'loginService','$modal', '
 	$scope.show = '';
 
 	$scope.open = function () {
-	  $scope.submitted = true;
+		$scope.submitted = true;
 
-	    var modalInstance = $modal.open({
-	      templateUrl: 'loginmodal.html',
-	      controller: 'LoginInstanceCtrl',
-		    size: 'sm',
-	      resolve: {
-	        show : function () {
-	          return $scope.show;
-	        }
-	      }
-	    });
+		var modalInstance = $modal.open({
+			templateUrl: 'loginmodal.html',
+			controller: 'LoginInstanceCtrl',
+			size: 'sm',
+			resolve: {
+				show : function () {
+					return $scope.show;
+				}
+			}
+		});
 
-	    modalInstance.result.then(function () {
+		modalInstance.result.then(function () {
 
-	    }, function () {
-	//      resets the signup/login view
-	      $scope.show = '';
-	    });
-	  };
-	}]);
+		}, function () {
+			//      resets the signup/login view
+			$scope.show = '';
+		});
+	};
+}]);
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 var LoginInstanceCtrl = function ($scope, $modalInstance, loginService, show, $facebook) {
 
-  $scope.signIn = function(){
-    var args={
-      email:    $scope.user.email,
-      password: $scope.user.password
-    };
+	$scope.signIn = function(){
+		var args={
+			email:    $scope.user.email,
+			password: $scope.user.password
+		};
 
-<<<<<<< HEAD
-//		Legacy sign function. Located in the legacy folder. remove when we move to new api
-	  loginService.Login(args).then(function(){
-	    $window.location.reload();
-    });
-
-//    New login function working. Uncomment when new api is ready
-//    loginService.Login(args)
-//      .then(function(){
-//          $modalInstance.close();
-//      });
-=======
 //	  Login api with notifications
-	  loginService.Login(args).then(function(data){
-		  if(data.success === true){
-			  $window.location.reload();
-		  }else{
+		loginService.Login(args).then(function(data){
+			if(data.success === true){
+				$window.location.reload();
+			}else{
 
-		  }
-	  });
->>>>>>> develop
-  };
+			}
+		});
+	};
 
 //	SignUp Function
 	$scope.signup = function(){
@@ -83,17 +70,17 @@ var LoginInstanceCtrl = function ($scope, $modalInstance, loginService, show, $f
 		$facebook.login()
 				.then(function() {
 
-			})
-		};
+				})
+	};
 
 	$scope.facebookSignUp = function(){
 		signUpSocialFacebook();
 	};
 
-  // Pass $scope.show to the modal instance. $scope. $scope.show is used for changing between signin and signup
-  $scope.show = show;
+	// Pass $scope.show to the modal instance. $scope. $scope.show is used for changing between signin and signup
+	$scope.show = show;
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
 };
