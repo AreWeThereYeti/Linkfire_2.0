80bbb1f17208d7ce084f723448de1ba5c8bd515a
Niels Peter Rasmussen <nprasmussen@gmail.com>
2014-08-04 14:13:49 +0200

    add src verification check DEV

--- dev/scripts/controllers/CreateBoardCtrl.js
+++ dev/scripts/controllers/CreateBoardCtrl.js
@@ -28,11 +28,23 @@ linkfireWebappApp.controller('CreateBoardCtrl', function ($scope, $http, $q, $fi
             if($scope.srcDest.indexOf("spotify.com") > -1){
                 $scope.logo1 = "spotify";
             }
-            else if($scope.srcDest.indexOf("itunes.com") > -1){
+            else if($scope.srcDest.indexOf("apple.com") > -1){
                 $scope.logo1 = "itunes";
             }
+            else if($scope.srcDest.indexOf("deezer.com") > -1){
+                $scope.logo1 = "deezer";
+            }
+            else if($scope.srcDest.indexOf("wimp.dk") > -1){
+                $scope.logo1 = "wimp";
+            }
+            else if($scope.srcDest.indexOf("soundcloud.com") > -1){
+                $scope.logo1 = "soundcloud";
+            }
+            else if($scope.srcDest.indexOf("beatsmusic.com") > -1){
+                $scope.logo1 = "beats";
+            }
             else {
-                $scope.logo1 = "no logo";
+                $scope.logo1 = "linkfire";
             }
 
 
@@ -43,11 +55,65 @@ linkfireWebappApp.controller('CreateBoardCtrl', function ($scope, $http, $q, $fi
 
     });
 
+    $scope.updateLogo() = function () {
+        if($scope.name){
+            if($scope.name.indexOf("spotify.com") > -1){
+                $scope.source.service = "spotify";
+            }
+            else if($scope.name.indexOf("apple.com") > -1){
+                $scope.source.service = "itunes";
+            }
+            else if($scope.name.indexOf("deezer.com") > -1){
+                $scope.service = "deezer";
+            }
+            else if($scope.name.indexOf("wimp.dk") > -1){
+                $scope.source.service = "wimp";
+            }
+            else if($scope.name.indexOf("soundcloud.com") > -1){
+                $scope.source.service = "soundcloud";
+            }
+            else if($scope.name.indexOf("beatsmusic.com") > -1){
+                $scope.source.service = "beats";
+            }
+        }
+        console.log("lol");
+    };
+
+
+    $scope.$watch('name', function() {
+        console.log("change");
+        if($scope.name){
+            if($scope.name.indexOf("spotify.com") > -1){
+                $scope.source.service = "spotify";
+            }
+            else if($scope.name.indexOf("apple.com") > -1){
+                $scope.source.service = "itunes";
+            }
+            else if($scope.name.indexOf("deezer.com") > -1){
+                $scope.service = "deezer";
+            }
+            else if($scope.name.indexOf("wimp.dk") > -1){
+                $scope.source.service = "wimp";
+            }
+            else if($scope.name.indexOf("soundcloud.com") > -1){
+                $scope.source.service = "soundcloud";
+            }
+            else if($scope.name.indexOf("beatsmusic.com") > -1){
+                $scope.source.service = "beats";
+            }
+
+
+
+        }
+
+
+    });
+
 
     $scope.addSrc = function () {
 
         $scope.sources.push({
-            service: $scope.src,
+            service: $scope.logo1,
             name: $scope.srcDest
         });
 
